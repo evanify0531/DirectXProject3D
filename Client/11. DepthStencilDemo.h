c9a8fd12896e3d894a75319a56265f0cb2e902eb
evanify0531 <evanify@yonsei.ac.kr>
2023-11-09 20:51:41 +0900

    11. DepthStencilDemo

---
+++ Client/11. DepthStencilDemo.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "IExecute.h"
+#include "Geometry.h"
+
+class DepthStencilDemo : public IExecute
+{
+public:
+	void Init() override;
+	void Update() override;
+	void Render() override;
+
+	shared_ptr<Shader> _shader;
+
+	//Object
+	shared_ptr<GameObject> _obj;
+	shared_ptr<GameObject> _obj2;
+
+
+	//Camera
+	shared_ptr<GameObject> _camera;
+
+	Vec3 _lightDir = Vec3(0.f, -1.f, 0.f);
+};
+
